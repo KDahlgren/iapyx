384fb96e59784413a0266fa11ab3cd4ee2a9a18f
Madeline Schade <madelineschade@gmail.com>
2018-01-16 12:22:16 -0800

    progress towards comb rewrite

---
+++ qa/Test_comb.py
@@ -0,0 +1,288 @@
+#!/usr/bin/env python
+
+'''
+Test_dml.py
+'''
+
+#############
+#  IMPORTS  #
+#############
+# standard python packages
+import inspect, logging, os, sqlite3, sys, unittest
+from StringIO import StringIO
+
+# ------------------------------------------------------ #
+# import sibling packages HERE!!!
+if not os.path.abspath( __file__ + "/../../src" ) in sys.path :
+  sys.path.append( os.path.abspath( __file__ + "/../../src" ) )
+
+from dedt       import dedt, dedalusParser, Fact, Rule, clockRelation, dedalusRewriter
+from utils      import dumpers, globalCounters, tools
+from evaluators import c4_evaluator
+
+import dml
+
+# ------------------------------------------------------ #
+
+
+##############
+#  TEST COMB  #
+##############
+class Test_comb( unittest.TestCase ) :
+
+  #logging.basicConfig( format='%(levelname)s:%(message)s', level=logging.DEBUG )
+  logging.basicConfig( format='%(levelname)s:%(message)s', level=logging.INFO )
+  #logging.basicConfig( format='%(levelname)s:%(message)s', level=logging.WARNING )
+
+  PRINT_STOP = True
+
+
+  ################
+  #  COMB REPLOG  #
+  ################
+  # tests rewriting replog
+  @unittest.skip( "working on different example" )
+  def test_dml_replog( self ) :
+
+    # specify input and output paths
+    inputfile               = os.getcwd() + "/testFiles/replog_driver.ded"
+    expected_iapyx_comb_path = "./testFiles/replog_iapyx_dml.olg"
+
+    self.comparison_workflow( inputfile, expected_iapyx_comb_path )
+
+  ################
+  # COMB SIMPLOG  #
+  ################
+  # tests rewriting simplog
+  # @unittest.skip( "working on different example" )
+  def test_dml_simplog( self ) :
+
+    # specify input and output paths
+    inputfile               = os.getcwd() + "/testFiles/simplog_driver.ded"
+    expected_iapyx_dml_path = "./testFiles/simplog_iapyx_dml.olg"
+
+    self.comparison_workflow( inputfile, expected_iapyx_dml_path )
+
+
+  ###############
+  #  COMB RDLOG  #
+  ###############
+  # tests rewriting rdlog
+  @unittest.skip( "working on different example" )
+  def test_comb_rdlog( self ) :
+
+    # specify input and output paths
+    inputfile               = os.getcwd() + "/testFiles/rdlog_driver.ded"
+    expected_iapyx_dml_path = "./testFiles/rdlog_iapyx_dml.olg"
+
+    self.comparison_workflow( inputfile, expected_iapyx_dml_path )
+
+  # ############################
+  # #  COMB TOY 3 AGG REWRITES  #
+  # ############################
+  # # tests rewriting the second toy program
+  @unittest.skip( "working on different example" )
+  def test_comb_toy3_aggRewrites( self ) :
+
+    # specify input and output paths
+    inputfile               = os.getcwd() + "/testFiles/toy3_aggRewrites.ded"
+    expected_iapyx_dml_path = "./testFiles/toy3_aggRewrites.olg"
+
+    self.comparison_workflow( inputfile, expected_iapyx_dml_path )
+
+  ###############
+  #  COMB TOY 2  #
+  ###############
+  # tests rewriting the second toy program
+  @unittest.skip( "working on different example" )
+  def test_comb_toy2( self ) :
+
+    # specify input and output paths
+    inputfile               = os.getcwd() + "/testFiles/toy2.ded"
+    expected_iapyx_dml_path = "./testFiles/toy2_comb.olg"
+
+    self.comparison_workflow( inputfile, expected_iapyx_dml_path )
+
+
+  #############
+  #  COMB TOY  #
+  #############
+  # tests rewriting the toy program
+  @unittest.skip( "working on different example" )
+  def test_comb_toy( self ) :
+
+    # specify input and output paths
+    inputfile               = os.getcwd() + "/testFiles/toy.ded"
+    expected_iapyx_dml_path = "./testFiles/toy_comb.olg"
+
+    self.comparison_workflow( inputfile, expected_iapyx_dml_path )
+
+  #########################
+  #  COMPARISON WORKFLOW  #
+  #########################
+  # defines iapyx dml comparison workflow
+  def comparison_workflow( self, inputfile, expected_iapyx_comb_path ) :
+
+    # --------------------------------------------------------------- #
+    # testing set up.
+
+    if os.path.isfile( "./IR.db" ) :
+      logging.debug( "  COMPARISON WORKFLOW : removing rogue IR file." )
+      os.remove( "./IR.db" )
+
+    testDB = "./IR.db"
+    IRDB    = sqlite3.connect( testDB )
+    cursor  = IRDB.cursor()
+
+    # --------------------------------------------------------------- #
+    # reset counters for new test
+
+    dedt.globalCounterReset()
+
+    # --------------------------------------------------------------- #
+    # runs through function to make sure it finishes with expected error
+
+    # get argDict
+    argDict = self.getArgDict( inputfile )
+
+    # run translator
+    programData = dedt.translateDedalus( argDict, cursor )
+
+    # portray actual output program lines as a single string
+    iapyx_results = self.getActualResults( programData[0] )
+
+    if self.PRINT_STOP :
+      print iapyx_results
+      expected_iapyx_results = None
+      sys.exit( "print stop." )
+
+    # ========================================================== #
+    # IAPYX COMPARISON
+    #
+    # grab expected output results as a string
+
+    expected_iapyx_results = None
+    with open( expected_iapyx_comb_path, 'r' ) as expectedFile :
+      expected_iapyx_results = expectedFile.read()
+    # self.assertEqual( iapyx_results, expected_iapyx_results )
+
+    # ========================================================== #
+    # EVALUATION COMPARISON
+
+    self.evaluate( programData )
+
+    # --------------------------------------------------------------- #
+    #clean up testing
+
+    IRDB.close()
+    os.remove( testDB )
+
+
+  ##################
+  #  GET ARG DICT  #
+  ##################
+  def getArgDict( self, inputfile ) :
+
+    # initialize
+    argDict = {}
+
+    # populate with unit test defaults
+    argDict[ 'prov_diagrams' ]            = False
+    argDict[ 'use_symmetry' ]             = False
+    argDict[ 'crashes' ]                  = 0
+    argDict[ 'solver' ]                   = None
+    argDict[ 'disable_dot_rendering' ]    = False
+    argDict[ 'settings' ]                 = "settings_comb.ini"
+    argDict[ 'negative_support' ]         = False
+    argDict[ 'strategy' ]                 = None
+    argDict[ 'file' ]                     = inputfile
+    argDict[ 'EOT' ]                      = 4
+    argDict[ 'find_all_counterexamples' ] = False
+    argDict[ 'nodes' ]                    = [ "a", "b", "c" ]
+    argDict[ 'evaluator' ]                = "c4"
+    argDict[ 'EFF' ]                      = 2
+
+    return argDict
+
+
+
+  ##############
+  #  EVALUATE  #
+  ##############
+  # evaluate the datalog program using some datalog evaluator
+  # return some data structure or storage location encompassing the evaluation results.
+  def evaluate( self, programData ) :
+
+    noOverlap = False
+
+    results_array = c4_evaluator.runC4_wrapper( programData )
+
+    # ----------------------------------------------------------------- #
+    # convert results array into dictionary
+
+    eval_results_dict = tools.getEvalResults_dict_c4( results_array )
+    # ----------------------------------------------------------------- #
+    # collect all pos/not_ rule pairs
+
+    rule_pairs = self.getRulePairs( eval_results_dict )
+
+    logging.debug( "  EVALUATE : rule_pairs = " + str( rule_pairs ) )
+
+    # ----------------------------------------------------------------- #
+    # make sure tables do not overlap
+
+    self.assertFalse( self.hasOverlap( rule_pairs, eval_results_dict ) )
+
+    # ----------------------------------------------------------------- #
+    # make sure dml positive relation results are identical to molly
+    # relation results
+
+    #
+
+    #################
+  #  HAS OVERLAP  #
+  #################
+  # make sure pos and not_pos tables do not overlap
+  def hasOverlap( self, rule_pairs, eval_results_dict ) :
+
+    for pair in rule_pairs :
+
+      logging.debug( "  HAS OVERLAP : pair = " + str( pair ) )
+
+      pos_results = eval_results_dict[ pair[0] ]
+      not_results = eval_results_dict[ pair[1] ]
+
+      for pos_row in pos_results :
+        if pos_row in not_results :
+          return True
+
+    return False
+
+  ########################
+  #  GET ACTUAL RESULTS  #
+  ########################
+  def getActualResults( self, programLines ) :
+    program_string  = "\n".join( programLines )
+    program_string += "\n" # add extra newline to align with read() parsing
+    return program_string
+
+    ####################
+  #  GET RULE PAIRS  #
+  ####################
+  # grab all pos/not_
+  def getRulePairs( self, eval_results_dict ) :
+
+    pair_list = []
+
+    # pull out positive names
+    for relName1 in eval_results_dict :
+
+      if not relName1.startswith( "not_" ) and not "_prov" in relName1 :
+
+        for relName2 in eval_results_dict :
+  
+          if not relName1 == relName2 :
+            if relName2.startswith( "not_" ) and relName2[4:] == relName1 :
+              pair_list.append( [ relName1, relName2 ] )
+
+    return pair_list
--- src/comb/combitorialNegRewriter.py
+++ src/comb/combitorialNegRewriter.py
@@ -18,86 +18,79 @@ from evaluators  import c4_evaluator
 import domain
 from negate import  negateRule
 
-def neg_rewrite(cursor):
+def neg_rewrite(cursor, ruleMeta, factMeta, parsedResults):
   ''' Performs the negative rewrite of the dedalus program. '''
-  original_prog = c4_translator.c4datalog( cursor )
-  for section in original_prog:
-    for line in section:
-      print line
-  results_array = c4_evaluator.runC4_wrapper( original_prog )
-  parsedResults = tools.getEvalResults_dict_c4( results_array )
-  print parsedResults
+
+  # add in active domain facts, this should only be done once in reality.
+  factMeta = domain.getActiveDomain(cursor, factMeta, parsedResults)
+
   while True:
-    print "running"
+
     original_prog = c4_translator.c4datalog( cursor )
-    print "----------------------------------------------------------------"
-    print "Program"
-    print "----------------------------------------------------------------"
     for line in original_prog[0]:
       print line
-    for line in original_prog[1]:
-      print line
-    print "----------------------------------------------------------------"
-    results_array = c4_evaluator.runC4_wrapper( original_prog )
-
+    # for line in original_prog[1]:
+    #   print line
+    # for line in original_prog[2]:
+    #   print line
     # run the program to get the new domains.
     # TODO: make this unnecessary, can be done if we just take in the 
     # original inputs maybe even better
-    rulesToNegate = findNegativeRules(cursor, parsedResults)
+    rulesToNegate = findNegativeRules(cursor, ruleMeta)
     rulesToNegateList = rulesToNegate.keys()
-
     # if there are no rules to negate, exit
     if len(rulesToNegateList) == 0:
       break
 
-    # add in active domain facts, this should only be done once in reality.
-    dom = domain.getActiveDomain(cursor, parsedResults)
-
     # Negate the rules in the list
-    negateRules(cursor, rulesToNegate, parsedResults)
-    setTypes.setTypes( cursor )
+    ruleMeta, factMeta = negateRules(cursor, ruleMeta, factMeta, rulesToNegate, parsedResults)
+    # setTypes.setTypes( cursor )
+  print len(ruleMeta), len(factMeta)
+  return ruleMeta, factMeta
 
-  # #####
-  # THIS IS TESTING CODE REMOVE IT
-  # #####
-  print "----------------------------------------------------------------"
-  print "done"
-  print "----------------------------------------------------------------"
-  original_prog = c4_translator.c4datalog( cursor )
-  results_array = c4_evaluator.runC4_wrapper( original_prog )
-  parsedResults = tools.getEvalResults_dict_c4( results_array )
-  print parsedResults
 
-def findNegativeRules(cursor, parsedResults):
+def findNegativeRules(cursor, ruleMeta):
   ''' finds all the rules with a 'notin' '''
-
-  #grab all ruleids in the program
-  cursor.execute("SELECT rid, goalName FROM Rule")
-  ridList = cursor.fetchall()
-
   rulesToNegate = {}
-  for rule in ridList:
-    if rule[1].startswith('dom'):
+  for rule in ruleMeta:
+    if rule.relationName.startswith('dom'):
       continue
-    # fetch all subgoals for a given rule.
-    cursor.execute('''SELECT s.sid, s.subgoalName, s.subgoalPolarity
-      FROM Rule subgoalRule
-      LEFT JOIN SubGoals s ON subgoalRule.goalName = s.subgoalName
-      LEFT JOIN Rule r ON s.rid = r.rid
-      WHERE r.goalName = "''' + rule[1] + '"')
-    SubGoals = cursor.fetchall()
-
-    # loop through all subgoals
-    for subGoal in SubGoals:
-      if subGoal[2] == 'notin':
-        # if the polarity is negative, add it to the list of goals to
-        # flip
-        rulesToNegate[str(subGoal[1])] = rule[1]
-
+    for subgoal in rule.subgoalListOfDicts:
+      if subgoal['polarity'] == 'notin':
+        if isRule(ruleMeta, subgoal['subgoalName']):
+          rulesToNegate[subgoal['subgoalName']] = rule.relationName
   return rulesToNegate
 
-def negateRules(cursor, rulesToNegate, parsedResults):
+def isRule(ruleMeta, ruleName):
+  for rule in ruleMeta:
+    if rule.relationName == ruleName:
+      return True
+  return False
+
+def negateRules(cursor, ruleMeta, factMeta, rulesToNegate, parsedResults):
   ''' Negates all rules '''
   for rule in rulesToNegate.iteritems():
     # for each rule, negate it.
-    negateRule(cursor, rule, parsedResults)
+    ruleMeta, factMeta = negateRule(cursor, rule, ruleMeta, factMeta, parsedResults)
+  return ruleMeta, factMeta
+
+def printRuleMeta(ruleMeta):
+  print "Rules"
+  for rule in ruleMeta:
+    print "***"
+    print rule.rid
+    print rule.relationName
+    print rule.goalAttList
+    print rule.goalTimeArg
+    print "subgoals"
+    for subgoal in rule.subgoalListOfDicts:
+      print subgoal
+
+def printFactMeta(factMeta):
+  print "Facts"
+  for fact in factMeta:
+    print "***"
+    print fact.fid 
+    print fact.relationName
+    print fact.factTimeArg
+    print fact.dataListWithTypes
